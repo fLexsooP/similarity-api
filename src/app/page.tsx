f6c2e309f93602f0bdf7055f6caedde7001ed98a
Chence Shi <ascc594557067@gmail.com>
2023-05-07 03:03:20 -0700

    add LargeHeading and Paragraph components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import Paragraph from './components/ui/Paragraph'
 
 export default function Home() {
   return (
     <main className="bg-red-500">
-      Hello
+
+      <Paragraph size='sm'>test text</Paragraph>
     </main>
   )
 }
