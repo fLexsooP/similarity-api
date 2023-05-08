b297fab7bc7bdc51544704247b8a81504feb969b
Chence Shi <ascc594557067@gmail.com>
2023-05-08 03:06:15 -0700

    restructure file and add navbar components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,27 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { Inter } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import Providers from '@/components/Providers';
 
-
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn('bg-white text-slate-900 antialiased', inter.className)}>
+      <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
+        <Providers>{children}
+          <Navbar />
+        </Providers>
+
+        {/* {children} */}
+        <div className='h-40 md:hidden' />
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+
+import Paragraph from '@/components/ui/Paragraph'
 import Image from 'next/image'
-import Paragraph from './components/ui/Paragraph'
+
+
 
 export default function Home() {
   return (
