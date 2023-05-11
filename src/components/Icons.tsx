5323e1811421ef47343405ccd015d0a498f8ac83
Chence Shi <ascc594557067@gmail.com>
2023-05-10 20:41:34 -0700

    fix: isolate icons

---
+++ src/components/Icons.tsx
@@ -0,0 +1,11 @@
+import { ChevronLeft, ChevronRight, Moon, Sun, User2 } from 'lucide-react'
+
+export const Icons = {
+  ChevronLeft,
+  ChevronRight,
+  Sun,
+  Moon,
+  User2,
+}
+
+export default Icons
